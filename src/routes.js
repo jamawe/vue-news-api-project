a16e34b56d2d1c814b2fa7eaabbe62399c895ab3
Jana Wernick <jana.wernick@gmail.com>
2022-08-31 16:56:34 +0200

    refactor: Remove unused view from routes

--- src/routes.js
+++ src/routes.js
@@ -1,28 +1,31 @@
 import HomePage from './pages/HomePage.vue';
 import CategoryPage from './pages/CategoryPage.vue';
 import ArticlePage from './pages/ArticlePage.vue';
-import SearchResultsPage from './pages/SearchResultsPage.vue';
+// import SearchResultsPage from './pages/SearchResultsPage.vue';
 
 export const routes = [
 
-  { path: '/',
+  {
+    path: '/',
     component: HomePage 
   },
 
-  { path: '/:category(business|entertainment|general|health|science|sports|technology)',
+  {
+    path: '/:category(business|entertainment|general|health|science|sports|technology)',
     name: 'CategoryPage',
     component: CategoryPage 
   },
 
-  { path: '/:category(business|entertainment|general|health|science|sports|technology)/:slug',
+  {
+    path: '/:category(business|entertainment|general|health|science|sports|technology)/:slug',
     name: 'ArticlePage',
     component: ArticlePage,
     props: true 
   },
 
-  { path: '/results',
-    component: SearchResultsPage 
-  },
+  // { path: '/results',
+  //   component: SearchResultsPage 
+  // },
   
 ]
 
