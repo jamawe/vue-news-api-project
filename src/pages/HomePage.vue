755aa63919beefcbd1d2329d9f1d5a0b8ead430f
Jana Wernick <jana.wernick@gmail.com>
2022-09-21 23:24:36 +0200

    feature: Add infinite scrolling functionality for HomePage + handle 429 Too Many Requests

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,13 +3,47 @@
     
     <AppHeader :headerTitle="headerTitle" />
 
-    <ArticleSingleSkeleton v-if="!loaded" />
+    <ArticleSingleSkeleton v-if="!loaded && !requestLimitExceeded" />
 
     <ArticleSingle
       v-for="(article, i) in articles"
       :key="i"
       :articleSingle="article"
     />
+
+    <template v-if="requestLimitExceeded">
+      <v-banner
+        rounded
+        max-width="600"
+        class="mx-auto mb-8">
+        The request limit has been exceeded. Your request will be repeated automatically in a few seconds and more articles will be displayed once it was successful.
+        <template v-slot:actions>
+          <v-btn
+            text
+            color="primary"
+          >
+            Dismiss
+          </v-btn>
+          <v-btn
+            text
+            disabled
+          >
+            Retry in 10
+          </v-btn>
+        </template>
+      </v-banner>
+
+      <ArticleSingleSkeleton />
+    </template>
+
+    <ArticleSingleSkeleton
+      v-if="showLazySkeleton"
+      v-intersect.quiet="{
+        handler: loadNextPage,
+        options: {
+          threshold: [1.0]
+        }
+      }" />
 
   </v-container>
 </template>
@@ -42,10 +76,16 @@ export default {
       articles: [],
       date: '',
       url: '',
-      newsDesk: '',
+      newsDesk: '"Culture" "Foreign" "Magazine" "Politics" "Sports"',
+      isSection: false,
+      docs: [],
+      status: '',
+      showLazySkeleton: false,
+      requestLimitTimeout: '',
+      requestLimitExceeded: false,
       // request + requestThreshold needed for limit how many pages to load
-      request: 0,
-      requestThreshold: 3,
+      request: 0, // TODO skip if 429 limit of api should be used instead (see below)
+      requestThreshold: 20, // TODO skip if 429 limit of api should be used instead (see below)
     }
   },
 
@@ -55,25 +95,97 @@ export default {
 
   methods: {
     async getArticles() {
-
-      // TODO: Only needed if repeatRequest fn stays
-      // this.date = new Date();
-      // const dateModified = modifyDateForApiRequest(this.date);
+      // Clear timeout if there was one from former request
+      if (this.requestLimitTimeout) clearTimeout(this.requestLimitTimeout);
+      
       const url = createApiRequest(this.newsDesk);
-      const { docs } = await getArticles(url);
-      this.request++;
-      this.articles.push(...modifyArticlesForDisplay(docs));
+
+      try {
+        const { docs } = await getArticles(url);
+        this.docs = docs;
+      } catch (e) {
+        // Handle 429, too many requests (>10 / 60sec)
+        if (e.response.status === 429) {
+          // Do not show skeleton anymore, but exceeded info banner instead
+          this.showLazySkeleton = false;
+          this.requestLimitExceeded = true;
+
+          // Retry same request after some time
+          this.requestLimitTimeout = setTimeout(() => {
+            this.getArticles();
+          }, 10000);
+        }
+        return e.response.status;
+      }
+      
+      // If request was successful
+      this.requestLimitExceeded = false;
+      this.request++; // TODO maybe skip, since mechanismn without threshold implemented (but keep for example for  CatgeoryPage)
+      this.articles.push(...modifyArticlesForDisplay(this.docs));
       this.loaded = true;
+
+      // Show skeleton that triggers new request on-intersect after some time
+      // Use setTimeout to avoid an immediate trigger
+      setTimeout(() => {
+        this.showLazySkeleton = true;
+      }, 3000);
     },
 
     async loadNextPage() {
-      while (this.request < this.requestThreshold) {
-        const url = createApiRequest(this.newsDesk, this.requestThreshold);
-        const { docs } = await getArticles(url);
-        this.articles = [...this.articles, ...modifyArticlesForDisplay(docs)];
+      // Clear timeout if there was one from former request
+      if (this.requestLimitTimeout) clearTimeout(this.requestLimitTimeout);
+
+      // TODO maybe skip (+else if below), since mechanismn without threshold implemented (but keep for example for  CatgeoryPage)
+      if (this.request < this.requestThreshold) {
+        console.log('%cINSIDE IF request, threshold', 'color: darkseagreen; font-weight: bold;', this.request, this.requestThreshold);
+        const url = createApiRequest(this.newsDesk, this.isSection, this.request);
+        try {
+          const { status, docs } = await getArticles(url);
+          this.status = status;
+          this.docs = docs;
+          console.log('%cstatus', 'color: hotpink; font-weight: bold;', status);
+        } catch (e) {
+          // Handle 429, too many requests (>10 / 60sec)
+          if (e.response.status === 429) {
+            // Do not show skeleton anymore, but exceeded info banner instead
+            this.showLazySkeleton = false;
+            this.requestLimitExceeded = true;
+
+            // Retry same request after some time
+            this.requestLimitTimeout = setTimeout(() => {
+              // Try first api request again after 5000ms
+              this.loadNextPage();
+            }, 10000);
+          }
+          return e.response.status;
+        }
+        this.requestLimitExceeded = false;
         this.request++;
-      } 
+        this.articles.push(...modifyArticlesForDisplay(this.docs));
+        this.showLazySkeleton = false;
+        
+        // TODO maybe skip, since mechanismn without threshold implemented (but keep for example for  CatgeoryPage)
+        // Show lazy skeleton again if own threshold is not exceeded
+        if (this.request < this.requestThreshold) {
+          // Show skeleton that triggers new request on-intersect after some time
+          // Use setTimeout to avoid an immediate trigger
+          setTimeout(() => {
+            this.showLazySkeleton = true;
+          }, 3000);
+        } 
+      
+      } else if (this.request >= this.requestThreshold) {
+        // Handle if of threshold got exceeded
+        // TODO maybe skip, since mechanismn without threshold implemented (but keep for example for  CatgeoryPage)
+        console.log('%cOWN threshold exceeded!!!', 'color: crimson; font-weight: bold;');
+        this.showLazySkeleton = false;
+      }
     },
+
+    // loadLazySkeleton() {
+    //   console.log('%cloadLazySkeleton run, showLazySkeleton', 'color: purple; font-weight: bold;', this.showLazySkeleton);
+    //   this.showLazySkeleton = true;
+    // },
 
     // TODO: Not sure if going one day back is still needed because of sort helper in api request (newest)
     // async repeatRequest() {
